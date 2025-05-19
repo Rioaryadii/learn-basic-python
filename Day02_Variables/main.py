5914b3c0c807790af1ae62e2999c5a46811d98e1
Rioaryadii <wiseriogt@gmail.com>
2025-05-19 15:59:14 +0700

    Day02: Variables & Data types

---
+++ Day02_Variables/main.py
@@ -0,0 +1,21 @@
+#Rio Fandia Aryadi
+#241011400059
+#02TPLE001
+
+#Day 02 - Variables & Data Types
+
+# Day 02 - Variables and Data Types
+
+def main():
+    name = "Rio Fandia Aryadi"
+    student_id = "241011400059"
+    class_code = "02TPLE001"
+    is_coding = True
+
+    print("Name:", name)
+    print("Student ID:", student_id)
+    print("Class Code:", class_code)
+    print("Learning Python?", is_coding)
+
+if __name__ == "__main__":
+    main()
