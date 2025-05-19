15d703a68f9a972b73bdf0898f4a02248455f2dc
Rioaryadii <wiseriogt@gmail.com>
2025-05-19 16:30:18 +0700

    Day03: Inputs & Outputs

---
+++ Day03_Inputs-Outputs/main.py
@@ -0,0 +1,13 @@
+#Rio Fandia Aryadi
+#241011400059
+#02TPLE001
+
+#Day 03 - Inputs & Outputs
+
+def main():
+    name = input("What is your name? ")
+    age = input("How old are you? ")
+    print(f"Hello, {name}! You are {age} years old.")
+
+if __name__ == "__main__":
+    main()
