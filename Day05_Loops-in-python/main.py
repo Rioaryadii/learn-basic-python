8ac3b3b26db3bb61dd3b6ea80e4ee4704979113d
Rioaryadii <wiseriogt@gmail.com>
2025-05-20 00:55:53 +0700

    Day05: Loops in Python

---
+++ Day05_Loops-in-python/main.py
@@ -0,0 +1,19 @@
+#Rio Fandia Aryadi
+#241011400059
+#02TPLE001
+
+#Day 05 - Loops in Python
+
+def main():
+    # For loop example
+    for i in range(5):
+        print(f"Iteration {i}")
+
+    # While loop example
+    count = 0
+    while count < 5:
+        print(f"Count is {count}")
+        count += 1
+
+if __name__ == "__main__":
+    main()
