f39efbc0d0adb475a83d4be80779de2b67064b04
Rioaryadii <wiseriogt@gmail.com>
2025-05-20 21:52:04 +0700

    Day07: Lists in Python

---
+++ Day07_Lists-in-python/main.py
@@ -0,0 +1,17 @@
+#Rio Fandia Aryadi
+#241011400059
+#02TPLE001
+
+#Day07 - Lists in Python
+
+def main():
+    pets = ["cat", "dog", "bird"]
+    pets.append("fish")
+    pets.remove("dog")
+
+    print("My pets are:")
+    for pet in pets:
+        print(pet)
+
+if __name__ == "__main__":
+    main()
