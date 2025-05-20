2890d15b7d86f3cc7952e5cc51f6eb51f734b93f
Rioaryadii <wiseriogt@gmail.com>
2025-05-20 21:40:23 +0700

    Day06: Functions in Python

---
+++ Day06_Functions/main.py
@@ -0,0 +1,17 @@
+#Rio Fandia Aryadi
+#241011400059
+#02TPLE001
+
+#Day 06 - Functions in Python
+def greet(name):
+    return f"Hello, {name}!"
+
+def add(a, b):
+    return a + b
+
+def main():
+    print(greet("Rio"))
+    print("5 + 9 =", add(5, 9))
+
+if __name__ == "__main__":
+    main()
