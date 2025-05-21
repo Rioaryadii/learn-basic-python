2ae174191e9fc128d073956b8ea6d003d4237c0a
Rioaryadii <wiseriogt@gmail.com>
2025-05-21 23:49:29 +0700

    Day09: Tuples in Python

---
+++ Day09_Tuples/main.py
@@ -0,0 +1,20 @@
+#Rio Fandia Aryadi
+#241011400059
+#02TPLE001
+
+#Day09 - Tuples in Python
+
+def main():
+    coordinates = (9.5, 10.9)
+    print("Coordinates:", coordinates)
+
+    print("X:", coordinates[0])
+    print("Y:", coordinates[1])
+
+    #Tuple unpacking
+    x, y = coordinates
+    print(f"Unpacked -> X: {x}, Y: {y}")
+
+if __name__ == "__main__":
+    main()
+    
