5f3050a1fc0f597f99cb87e364f9dff829068b85
Rioaryadii <wiseriogt@gmail.com>
2025-05-21 23:43:00 +0700

    Day08: Dictionaries in Python

---
+++ Day08_Dictionaries/main.py
@@ -0,0 +1,21 @@
+#Rio Fandia Aryadi
+#241011400059
+#02TPLE001
+
+#Day08 - Dictionaries in Python
+
+def main():
+    students = {
+        "Name": "Rio",
+        "NIM": "241011400059",
+        "Class": "02TPLE001",
+        "Age": 20,
+        "Hobby": "Reading",
+        "Major": "Computer Science"
+    }
+    print("Student Information:")
+    for key, value in students.items():
+        print(f"{key}: {value}")
+    
+if __name__ == "__main__":
+    main()
